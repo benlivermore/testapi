5e70a25a12b454758d3f115dfeb6770df7834496
Ben Livermore <ben.b.livermore@gmail.com>
2014-11-29 07:26:10 -0500

    add cluster support to node app

--- server.js
+++ server.js
@@ -1,5 +1,29 @@
 var app = require('./app'),
 	port = 3000,
-	mongoPath = 'mongodb://localhost/testapi';
+	mongoPath = 'mongodb://localhost/testapi',
+	cluster = require('cluster');
 
-app.start(mongoPath, port);
+function startWorker() {
+	var worker = cluster.fork();
+	console.log('worker %d started', worker.id);
+}
+
+if(cluster.isMaster) {
+	require('os').cpus().forEach(function () {
+		startWorker();
+	});
+
+	cluster.on('disconnect', function (worker) {
+		console.log('worker %d was disconnected', worker.id);
+	});
+
+	cluster.on('exit', function (worker, code, signal) {
+		console.log('worker %d died with exit code %d (%s)', worker.id, code, signal);
+		startWorker();
+	});
+	
+} else {
+	app.start(mongoPath, port);
+}
+
+
